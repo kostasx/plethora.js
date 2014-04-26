2797505da0c07094de0111d430d86fccfb6b2684
Kostas <kostas@creativecollective.gr>
2014-04-26 12:26:03 +0300

    Update plethora.js

--- plethora.js
+++ plethora.js
@@ -1,44 +1,30 @@
 (function(){
 
 	var $_ = {};
+	var version = '1.0.0';
 
 	$_.iterate = $_.itr = function ( object, callback, own ){
 
 		if ( !callback ) throw new Error("You must provide a callback function.");
-
 		own = ( own !== false );
-
 		var key;
 		var hasOwnProp = Object.prototype.hasOwnProperty;
 
 		for ( key in object ) {
-
 			if ( 'object' === typeof object[key] ){
-
 				$_.iterate( object[key], callback, own );
-
 				continue;
-
 			}
 
 			if ( own ) {
-
 				if ( hasOwnProp.call(object, key) ){
-
 					callback( key, object[key] );
-
 				}
-
 			} else {
-
 				callback( key, object[key] );
-
 			}
-
 		}
-
 	}
-
 	this.plethora = this.$_ = $_;
 
 }).call(this);
